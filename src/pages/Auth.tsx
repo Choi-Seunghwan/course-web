e2c5c96aa325f5499f455d47ff14a074579ca49c
최승환 (Seunghwan Choi) <seunghwan@kyobodts.co.kr>
2025-02-12 01:32:35 +0900

    feat: 본인인증 모듈 테스트

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { portoneIdentityVerification } from "../utils/portoneVerification";
 
 const Container = styled.div`
   display: relative;
 `;
 
+const VerificationButton = styled.button`
+  width: 200px;
+  height: 50px;
+`;
+
 export default function Auth() {
-  return <Container></Container>;
+  const handleVerification = () => {
+    portoneIdentityVerification();
+  };
+
+  return (
+    <Container>
+      <div></div>
+      <VerificationButton
+        onClick={() => {
+          handleVerification();
+        }}
+      >
+        본인인증
+      </VerificationButton>
+    </Container>
+  );
 }
---
+++ src/utils/portoneVerification.ts
@@ -0,0 +1,14 @@
+import PortOne from "@portone/browser-sdk/v2";
+import { nanoid } from "nanoid";
+
+export const portoneIdentityVerification = async () => {
+  console.log("본인인증을 시작합니다.");
+
+  const result = await PortOne.requestIdentityVerification({
+    storeId: `${process.env.REACT_APP_PORTONE_STORE_ID}`,
+    identityVerificationId: `identity-verification-${nanoid()}`,
+    channelKey: `${process.env.REACT_APP_PORTONE_CHANNEL_KEY}`,
+  });
+
+  console.log("본인인증 결과:", result);
+};
