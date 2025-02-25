fd8b07cb092f5c5ed2795f12f8dd4efe13c0f865
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-02-25 17:06:03 +0900

    feat: sighup step

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -44,46 +44,51 @@ export default function SignUp() {
   };
 
   const handleSignUp = async () => {
-    await authSignUp();
+    // await authSignUp();
     setStep(2);
+  };
+
+  const renderStepContent = () => {
+    switch (step) {
+      case 0:
+        return (
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleVerification}
+            >
+              본인인증 하기
+            </Button>
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <p>정보 입력 페이지</p>
+            <Button variant="contained" color="primary" onClick={handleSignUp}>
+              완료
+            </Button>
+          </div>
+        );
+      case 2:
+        return <p>회원가입이 완료되었습니다!</p>;
+      default:
+        return <p>잘못된 접근입니다.</p>;
+    }
   };
 
   return (
     <Container>
-      <StepWrap>
-        <Stepper style={{ width: "100%" }} activeStep={step} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </StepWrap>
+      <Stepper style={{ width: "100%" }} activeStep={step} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
 
-      <SignUpWrap>
-        <Step1Div>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              handleVerification();
-            }}
-          >
-            본인인증 하기
-          </Button>
-        </Step1Div>
-        <Step2Div>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              handleSignUp();
-            }}
-          >
-            완료
-          </Button>
-        </Step2Div>
-      </SignUpWrap>
+      <div>{renderStepContent()}</div>
     </Container>
   );
 }
